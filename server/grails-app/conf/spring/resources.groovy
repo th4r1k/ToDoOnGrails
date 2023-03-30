fb89b9543499341b9a7b554e48ea79b0d59cf0b8
Tharik Honda <th4r1k@gmail.com>
2023-03-30 08:47:23 -0300

    feat: swagger integration

--- server/grails-app/conf/spring/resources.groovy
+++ server/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,16 @@
-// Place your Spring DSL code here
+import io.swagger.jaxrs.config.BeanConfig
+
 beans = {
+    swaggerConfig(BeanConfig) {
+        def serverUrl = "http://localhost:8080/"
+        def hostName = "localhost:8080"
+        resourcePackage = "todoongrails"
+        host = hostName
+        basePath = "/"
+        version = 'v0'
+        title = 'Swagger'
+        description = 'API for Accessing  resources'
+        contact = 'honda.tharik@gmail.com'
+        license = ''
+    }
 }
--- server/grails-app/controllers/todoongrails/UrlMappings.groovy
+++ server/grails-app/controllers/todoongrails/UrlMappings.groovy
@@ -13,5 +13,8 @@ class UrlMappings {
         "/"(controller: 'application', action:'index')
         "500"(view: '/error')
         "404"(view: '/notFound')
+
+        "/api" (controller: 'Swagger' , action: 'swaggerJson')
+
     }
 }
